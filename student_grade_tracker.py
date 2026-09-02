8a6d4315494b071569e515589df7ae35eab101e1
zumralsudekilic-gif <zumralsudekilic@gmail.com>
2026-09-03 01:41:16 +0300

    Create student_grade_tracker.py
    Add initial student grade tracker functionality

---
+++ student_grade_tracker.py
@@ -0,0 +1,31 @@
+students = []
+
+while True:
+    print("\n---- Student Grade Tracker ---")
+    print("1. Add Student")
+    print("2. Display Students")
+    print("3. Calculate Average")
+    print("4. Find Highest Grade")
+    print("5. Check Pass/Fail Status")
+    print("6. Exit")
+
+    choice = input("Chooese an option (1-6):  ")
+
+    if choice == "1":
+        name = input("Enter student name: ")
+        grade = float(input("Enter student grade: "))
+
+        student = {
+            "name": name,
+            "grade": grade
+        }
+
+        students.append(student)
+
+        print("Student successfully added!") 
+
+elif choice == "2"
+    if len(students) == 0:
+
+      
+  
